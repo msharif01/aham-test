f9910c331f1d09729aa23c01be2801a0ca5a4222
sharif <sharif@paywithsplit.co>
2023-06-28 17:29:35 +0800

    chores: cleanup

--- src/app/components/userListTable/UserListTable.tsx
+++ src/app/components/userListTable/UserListTable.tsx
@@ -5,11 +5,6 @@ import ActionDropdown from '../actiondropdown/ActionDropdown';
 import { createContext } from 'react';
 import {User} from '@/app/models/User';
 
-const users = [
-  { accountNumber: "001", name: "person A", email: 'personA@gmail.com', phoneNumber: '0111111111' },
-  { accountNumber: "002", name: "person B", email: 'personB@gmail.com', phoneNumber: '0122222222' },
-];
-
 export type UserContextType = {
   name: string;
   email: string;
@@ -31,36 +26,6 @@ type UserListTableProps = {
 
 export default function UserListTable({data}: UserListTableProps) {
   
-  const peopleA = [
-    {
-        "_id": "649bd0dad21e0abe07cd1ee1",
-        "name": "deadlock",
-        "email": "deadlockk@gmail.com",
-        "phoneNumber": "0111234567",
-    },
-    {
-        "_id": "949bp9dad21e0ibe87cd1ee1",
-        "name": "chamber",
-        "email": "chamber@gmail.com",
-        "phoneNumber": "014434567",
-    },
-  ];
-
-  const peopleB = [
-    {
-        _id: "649bd0dad21e0abe07cd1ee1",
-        name: "deadlock",
-        email: "deadlockk@gmail.com",
-        phoneNumber: "0111234567",
-    },
-    {
-        _id: "949bp9dad21e0ibe87cd1ee1",
-        name: "chamber",
-        email: "chamber@gmail.com",
-        phoneNumber: "014434567",
-    },
-  ];
-
   const rows = data.map((user) => (
     <tr key={user.email}>
       <td>{user.name}</td>
